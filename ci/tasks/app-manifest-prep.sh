c3c1014ca436d7388ad2bf948121553c8fba8097
goprasad <gorle.prasad@gmail.com>
2018-05-16 20:03:10 +0000

    parmeterized the host app-manifest prep

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,5 +4,5 @@ set -xe
 Version=`cat version/number`
 
 
-echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  host: gorle-org-Music-CICD-Demo \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  host:${cf_app_host}  \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
 
